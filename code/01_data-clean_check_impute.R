1f3cfab2f67fe641f25203ce6bdaabff95b8a788
Keith <kpost342005@hotmail.com>
2024-06-04 20:50:52 -0600

    Final and, where necessary, interim tables and figures were created to facilitate report writing

--- code/01_data-clean_check_impute.R
+++ code/01_data-clean_check_impute.R
@@ -142,6 +142,17 @@ df_house <- initial_clean_house(df_house0)
 #   set_names(nm_isect) #none for all
 #----------------------------------------------------------------------------
 
+## Show variable classes
+tab_var_class <- df_house %>%
+  purrr::map(function(x) {
+    class(x)[[1]]
+  }) %>%
+  enframe(name="variable", value="class") %>%
+  unnest(class) %>%
+  arrange(class, variable)
+
+tab_var_class 
+
 
 # Preliminary Data Checking=========================================================================
 #basics
@@ -152,9 +163,13 @@ summary(df_house)
 
 
 #skim by type: chr, factor, numeric
-skim_by_type(data=df_house, fn=is.character)
-skim_by_type(data=df_house, fn=is.factor)
-skim_by_type(data=df_house, fn=is.numeric)
+tab_train_chr <- skim_by_type(data=df_house, fn=is.character)
+tab_train_fct <- skim_by_type(data=df_house, fn=is.factor)
+tab_train_num <- skim_by_type(data=df_house, fn=is.numeric)
+
+tab_train_chr
+tab_train_fct 
+tab_train_num 
 
 # skim(df_house, where(is.character)) %>%
 #   as_tibble() %>% 
@@ -182,28 +197,37 @@ skim_by_type(data=df_house, fn=is.numeric)
 ### Visualize missing data
 #all data
 vis_dat(df_house) 
-vis_miss(df_house, 
-         sort_miss=TRUE, 
-         cluster=TRUE) 
+
+fig_train_overall_miss <- vis_miss(df_house, sort_miss=TRUE, cluster=TRUE) 
+fig_train_overall_miss
 #0.2 % missing
 
 #cols with at least one NA
-df_house %>%
+fig_train_sub_miss <- df_house %>%
   select(where(~any(is.na(.)))) %>%
   vis_miss(sort_miss=TRUE,
-           cluster=TRUE)  
+           cluster=TRUE) 
+
+fig_train_sub_miss
 #for cols where at least one value is NA, total of 3.2% of values are missing
 
+#missing value dot plot
 df_house %>%
   gg_miss_var() 
 #lot_frontage by far has most NAs followed by the two mas_ cols, then three others
 
-df_house %>%
+#missing values by row
+fig_train_miss_row <- df_house %>%
   gg_miss_case() 
+
+fig_train_miss_row 
 #by far the cases with missing values have only one value missing
 
-df_house %>%
+#missing value patterns
+fig_train_miss_patt <- df_house %>%
   gg_miss_upset(nsets=6)
+
+fig_train_miss_patt 
 #257/259 lot_frontage NA values occur without other missing values
 #2/259 co-occur with mas_vnr_type and mas_vnr_area
 #6/8 mas_vnr_type & mas_vnr_area occur without other vars
@@ -212,12 +236,15 @@ df_house %>%
 df_house %>%
   missing_plot() 
 
+
 #### Examine missingness
-map_int(df_house, function(x) sum(is.na(x))) %>%
+tab_train_miss <- map_int(df_house, function(x) sum(is.na(x))) %>%
   enframe(name="predictor", value="n_na") %>%
   filter(n_na > 0) %>%
   mutate(pct_na=(n_na/1460) * 100) %>%
   arrange(desc(n_na)) 
+
+tab_train_miss 
 #6 cols with at least one NA
 #3 cols with one NA
 #1 col > .6% missing (lot_frontage)
@@ -230,29 +257,35 @@ map_int(df_house, function(x) sum(is.na(x))) %>%
 
 #### lot_frontage
 #compare distribution of y for cases when lot_frontage is present vs missing
-df_house %>%
+fig_sp_dist_lt_miss <- df_house %>%
   mutate(na_lot_frontage=is.na(lot_frontage)) %>%
   ggplot() +
   geom_histogram(aes(x=sale_price)) +
-  facet_wrap(~na_lot_frontage)
-#similar distributions
+  facet_wrap(~na_lot_frontage, nrow=2)
+
+fig_sp_dist_lt_miss
+#similar distributions...but 'FALSE' plot is more skewed and different peak than 'TRUE' plot
 
 #compare to a more closely related x variable: lot_shape
-df_house %>%
+fig_ls_dist_lt_miss <- df_house %>%
   mutate(na_lot_frontage=is.na(lot_frontage)) %>%
   ggplot() +
   geom_bar(aes(x=lot_shape)) +
-  facet_wrap(~na_lot_frontage)
+  facet_wrap(~na_lot_frontage, nrow=2)
+
+fig_ls_dist_lt_miss
 #clearly distribution differs based on presence/absence of lot_frontage data
 #evidence that it's not MCAR
 
 #### mas_vnr_type (and ._area as they have the same missingness)
 #8/1460 is very small but this suggests that it's not MCAR
-df_house %>%
+fig_foundation_dist_mvt <- df_house %>%
   mutate(na_mas_vnr_type=is.na(mas_vnr_type)) %>%
   ggplot() +
   geom_bar(aes(x=foundation)) +
-  facet_wrap(~na_mas_vnr_type)
+  facet_wrap(~na_mas_vnr_type, nrow=2)
+
+fig_foundation_dist_mvt
 #all poured concrete
   
 #clearly, these variables are not mcar and thus we'll use a multivariate imputation technique
@@ -268,11 +301,13 @@ df_house %>%
 
 ### Compare pre- and post-imputation
 #data: presence vs absence
-vis_compare(df_house, df_house_i) #for report--trim predictors to first x chr
+fig_train_pre_post_impute <- vis_compare(df_house, df_house_i) 
+fig_train_pre_post_impute
+
 
 #lot_frontage
 #visually
-df_house_i %>%
+fig_train_lf_pre_post_impute <- df_house_i %>%
   select(id, lot_frontage_imp="lot_frontage", sale_price) %>%
   left_join(df_house %>%
               select(id, lot_frontage), 
@@ -285,11 +320,13 @@ df_house_i %>%
   geom_point(alpha=0.2) +
   geom_smooth(method="lm", color="blue") +
   scale_color_viridis_d(end=0.5) +
-  facet_wrap(~lf_type) +
+  facet_wrap(~lf_type, nrow=2) +
   labs(x="Lot frontage (log) (ft)",
        y="Sale price ($)") +
   theme_bw() +
   theme(legend.position="none")
+
+fig_train_lf_pre_post_impute
   
 #statistically
 lf_nnas <- !is.na(df_house$lot_frontage)
