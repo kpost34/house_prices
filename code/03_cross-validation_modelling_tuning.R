cabe6623b99e10a0718f55e42304766ac7554904
Keith <kpost342005@hotmail.com>
2024-06-07 17:10:12 -0600

    Created a combined cv metrics table for report; turned the results of rf model tuning plot into an object; made vi() and vip() results into a table and figure, respectively; and saved objects to RDS files to save loading time in qmd

--- code/03_cross-validation_modelling_tuning.R
+++ code/03_cross-validation_modelling_tuning.R
@@ -50,7 +50,8 @@ fit_rs_dt <- wf_dt %>%
 
 
 ### Assess performance
-collect_metrics(fit_rs_dt)
+df_metrics_dt <- collect_metrics(fit_rs_dt)
+df_metrics_dt
 
 
 ## Random Forest--------------------
@@ -74,7 +75,8 @@ fit_rs_rf <- wf_rf %>%
 
 
 ### Assess performance
-collect_metrics(fit_rs_rf)
+df_metrics_rf <- collect_metrics(fit_rs_rf)
+df_metrics_rf
 
 
 ## K-nearest Neighbors--------------------
@@ -98,7 +100,25 @@ fit_rs_knn <- wf_knn %>%
 
 
 ### Assess performance
-collect_metrics(fit_rs_knn)
+df_metrics_knn <- collect_metrics(fit_rs_knn)
+df_metrics_knn
+
+
+## Combine metrics tables for report--------------------
+tab_model_cv_metrics <- list(
+  "decision_tree"=df_metrics_dt, 
+  "random_forest"=df_metrics_rf, 
+  "k-nearest neighbor"=df_metrics_knn
+  ) %>%
+  bind_rows(.id="model") %>%
+  select(!c(.estimator, .config)) %>%
+  pivot_wider(names_from=".metric", values_from=c("mean", "n", "std_err"), 
+              names_glue="{.metric}_{.value}", names_vary="slowest") %>%
+  select(!rsq_n) %>%
+  relocate(n="rmse_n", .after="model") %>%
+  arrange(rmse_mean)
+
+tab_model_cv_metrics
 
 
 
@@ -181,7 +201,14 @@ fit_rs_tune_rf <- wf_tune_rf %>%
 
 
 ### Assess performance
-collect_metrics(fit_rs_tune_rf)
+tab_cv_metrics_tune_rf <- collect_metrics(fit_rs_tune_rf) %>%
+  select(-.estimator) %>%
+  pivot_wider(names_from=.metric, values_from=c(mean, n, std_err),
+              names_vary="slowest", names_glue="{.metric}_{.value}") %>% 
+  select(trees, min_n, n="rmse_n", rmse_mean, rmse_std_err, rsq_mean, rsq_std_err, .config) %>%
+  arrange(rmse_mean)
+  
+tab_cv_metrics_tune_rf
 
 fit_rs_tune_rf %>%
   show_best(metric="rsq")
@@ -237,7 +264,7 @@ fit_rs_tune_knn %>%
 
 # Plot Results of Tuned Models======================================================================
 #random forest models as they generally performed better
-fit_rs_tune_rf %>%
+fig_metric_tune_rf <- fit_rs_tune_rf %>%
   collect_metrics() %>%
   mutate(across(.cols=c("trees", "min_n"), ~as.factor(.x))) %>%
   ggplot(aes(x=trees, y=mean, color = min_n)) +
@@ -247,6 +274,8 @@ fit_rs_tune_rf %>%
   scale_color_viridis_d(option = "plasma", end = .7) +
   theme_bw()
 #recall that best parameters: trees = 2000, min_n = 2
+
+fig_metric_tune_rf
 
 fit_rs_tune_rf %>%
   show_best(metric="rsq")
@@ -277,18 +306,43 @@ final_fit <- fit(wf_rf_final, df_house_train) %>%
 ## Model diagnostics
 final_fit
 
-vi(final_fit) 
-vip(final_fit, num_features=20)
+tab_vi_rf_final_model <- vi(final_fit) 
+tab_vi_rf_final_model
+
+fig_vip_rf_final_model <- vip(final_fit, num_features=20) 
+fig_vip_rf_final_model
 
 
+## Show relationship between overall_qual and sale_price
+fig_overall_qual_sale_price_box <- df_house_train %>%
+  ggplot() +
+  geom_boxplot(aes(x=overall_qual, y=sale_price)) +
+  theme_bw()
 
-# Write Model to File===============================================================================
-model_fp <- here("models", "final_model.rds")
-
-# saveRDS(final_fit, model_fp)
+fig_overall_qual_sale_price_box 
 
 
+# Write Modelling Objects to Files===================================================================
+## Create file paths
+model_cv_metrics_fp <- here("modelling", "mod_cv_metrics.rds")
+grid_rf_fp <- here("modelling", "grid_rf.rds")
+cv_metrics_tune_rf_fp <- here("modelling", "metrics_tune_rf.rds")
+metric_tune_rf_plot_fp <- here("modelling", "metric_tune_rf_plot.rds")
+final_model_fp <- here("modelling", "final_model.rds")
+vi_final_model_fp <- here("modelling", "vi_final_model.rds")
+vip_final_model_fp <- here("modelling", "vip_final_model.rds")
+overall_qual_sale_price_plot_fp <- here("modelling", "overall_qual_sale_price_plot.rds")
 
+
+## Save files
+saveRDS(tab_model_cv_metrics, model_cv_metrics_fp)
+saveRDS(grid_rf, grid_rf_fp)
+saveRDS(tab_cv_metrics_tune_rf, cv_metrics_tune_rf_fp)
+saveRDS(fig_metric_tune_rf, metric_tune_rf_plot_fp)
+saveRDS(final_fit, final_model_fp)
+saveRDS(tab_vi_rf_final_model, vi_final_model_fp)
+saveRDS(fig_vip_rf_final_model, vip_final_model_fp)
+saveRDS(fig_overall_qual_sale_price_box, overall_qual_sale_price_plot_fp)
 
 
 
